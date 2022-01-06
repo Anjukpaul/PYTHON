9921d9c36a2b2b226590ed1c72f3177fe171fe49
Anjukpaul <95705730+Anjukpaul@users.noreply.github.com>
2022-01-06 14:52:05 +0530

    Add files via upload

---
+++ string3.py
@@ -0,0 +1,7 @@
+def exchange(str):
+    return str[-1:] + str[1: -1] + str[:1]
+str=input("enter the string:")
+print("string after exchange")
+print(exchange(str))
+
+
---
+++ string5.py
@@ -0,0 +1,10 @@
+n = input("Enter the string:")
+if n[-3:] == 'ing':
+   n = n+'ly'
+else:
+   n = n+'ing'
+print(n)
+
+
+
+
